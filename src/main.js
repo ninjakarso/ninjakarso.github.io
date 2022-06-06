00c7ef896334258cf99f446b7cd2bbc5249dfc79
ninjakarso <106937691+ninjakarso@users.noreply.github.com>
2022-06-05 21:23:18 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,37 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '');
-game.state.add('play', {
-    preload: function() {
-    },
-    create: function() {
-
-    },
-    render: function() {
-        game.debug.text('Adventure Awaits!', 250, 290);
+var config = {
+    width: 800,
+    height: 600,
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    scene: {
+        create: create
     }
-});
-game.state.start('play');
+};
+
+var game = new Phaser.Game(config);
+
+function create ()
+{
+    var graphics = this.add.graphics({ lineStyle: { width: 2, color: 0x00ff00 }, fillStyle: { color: 0xff00ff } });
+
+    var circle = new Phaser.Geom.Circle(40, 575, 25);
+
+    function drawCircle ()
+    {
+        var area = Phaser.Geom.Circle.Area(circle);
+
+        graphics.fillRect(circle.right + 5, circle.bottom, 50,  -area / 50);
+        graphics.strokeCircleShape(circle);
+    }
+
+    drawCircle(graphics, circle);
+
+    circle.setTo(175, 550, 50);
+    drawCircle(graphics, circle);
+
+    circle.setTo(360, 525, 75);
+    drawCircle(graphics, circle);
+
+    circle.setTo(595, 500, 100);
+    drawCircle(graphics, circle);
+}
